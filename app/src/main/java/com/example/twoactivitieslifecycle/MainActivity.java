a3d4658262f7c4bee7ad6ecfa9b3d214c85852fd
mancy1998 <deepmansi2017@gmail.com>
2021-03-18 09:14:00 +0545

    added

--- app/src/main/java/com/example/twoactivitieslifecycle/MainActivity.java
+++ app/src/main/java/com/example/twoactivitieslifecycle/MainActivity.java
@@ -1,18 +1,20 @@
 package com.example.twoactivitieslifecycle;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG = MainActivity.class.getSimpleName() ;
-    public static final String EXTRA_MESSAGE = "com.example.twoactivitieslifecycle.extra.MESSAGE";
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE
+            = "com.example.twoactivitieslifecycle.extra.MESSAGE";
     public static final int TEXT_REQUEST = 1;
+
     private EditText mMessageEditText;
     private TextView mReplyHeadTextView;
     private TextView mReplyTextView;
@@ -23,6 +25,25 @@ public class MainActivity extends AppCompatActivity {
         Log.d(LOG_TAG, "onStart");
     }
 
+    /**
+     * Maintains the Activity state across configuration changes.
+     *
+     * @param outState Activity state data to save
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mReplyHeadTextView.getVisibility() == View.VISIBLE) {
+            outState.putBoolean("reply_visible", true);
+            outState.putString("reply_text", mReplyTextView.getText().toString());
+        }
+    }
+
+    /**
+     * Initializes the activity.
+     *
+     * @param savedInstanceState The current state data
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,36 +52,58 @@ public class MainActivity extends AppCompatActivity {
         Log.d(LOG_TAG, "-------");
         Log.d(LOG_TAG, "onCreate");
 
-//        get the views from their IDs
         mMessageEditText = findViewById(R.id.editText_main);
         mReplyHeadTextView = findViewById(R.id.text_header_reply);
         mReplyTextView = findViewById(R.id.text_message_reply);
 
+        if (savedInstanceState != null) {
+            boolean isVisible = savedInstanceState
+                    .getBoolean("reply_visible");
+            if (isVisible) {
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(savedInstanceState
+                        .getString("reply_text"));
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
     }
 
+    /**
+     * Handles the onClick for the "Send" button. Gets the value of the main
+     * EditText, creates an intent, and launches the second activity with
+     * that intent.
+     *
+     * The return intent from the second activity is onActivityResult().
+     *
+     * @param view The view (Button) that was clicked.
+     */
     public void launchSecondActivity(View view) {
-//        add message to debug logs
-        Log.d(LOG_TAG, "Button Clicked!");
-//        create intent, get string from EditText, put it in intent extras and send to SecondActivity
+        Log.d(LOG_TAG, "Button clicked!");
         Intent intent = new Intent(this, SecondActivity.class);
         String message = mMessageEditText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
-//       receive result back from SecondActivity
         startActivityForResult(intent, TEXT_REQUEST);
     }
 
-    //    method callback when SecondActivity returns result
-//    request code is the one used in startActivityForResult(); resultCode is the code which is received (can be RESULT_OK or RESULT_CANCELED; data is the intent which carries the data
+    /**
+     * Handles the data in the return intent from SecondActivity.
+     *
+     * @param requestCode Code for the SecondActivity request.
+     * @param resultCode Code that comes back from SecondActivity.
+     * @param data Intent data sent back from SecondActivity.
+     */
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == TEXT_REQUEST){
-            if (resultCode == RESULT_OK){
-//                get the reply message from SecondActivity, put the information in TextView and make them visible
+
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
                 String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+
                 mReplyHeadTextView.setVisibility(View.VISIBLE);
-                mReplyTextView.setVisibility(View.VISIBLE);
+
                 mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
             }
         }
     }
@@ -95,6 +138,4 @@ public class MainActivity extends AppCompatActivity {
         Log.d(LOG_TAG, "onDestroy");
     }
 }
-
-
 
--- app/src/main/java/com/example/twoactivitieslifecycle/SecondActivity.java
+++ app/src/main/java/com/example/twoactivitieslifecycle/SecondActivity.java
@@ -1,19 +1,20 @@
 package com.example.twoactivitieslifecycle;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class SecondActivity extends AppCompatActivity {
 
+public class SecondActivity extends AppCompatActivity {
     private static final String LOG_TAG
             = SecondActivity.class.getSimpleName();
+    public static final String EXTRA_REPLY =
+            "com.example.twoactivitieslifecycle.extra.REPLY";
 
-    public static final String EXTRA_REPLY = "com.example.twoactivitieslifecycle.extra.REPLY";
     private EditText mReply;
 
     @Override
@@ -22,19 +23,36 @@ public class SecondActivity extends AppCompatActivity {
         Log.d(LOG_TAG, "onStart");
     }
 
+    /**
+     * Initializes the activity.
+     *
+     * @param savedInstanceState The current state data.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
         mReply = findViewById(R.id.editText_second);
+
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView text = findViewById(R.id.text_message);
-        text.setText(message);
+
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
     }
 
+    /**
+     * Handles the onClick for the "Reply" button. Gets the message from the
+     * second EditText, creates an intent, and returns that message back to
+     * the main activity.
+     *
+     * @param view The view (Button) that was clicked.
+     */
     public void returnReply(View view) {
         String reply = mReply.getText().toString();
+
+
         Intent replyIntent = new Intent();
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK, replyIntent);
@@ -72,5 +90,4 @@ public class SecondActivity extends AppCompatActivity {
         Log.d(LOG_TAG, "onDestroy");
     }
 }
-
 
